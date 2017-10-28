c71925b1f1b4a2f313e33b851a6f0546c6ab037f
Ryan Chen <ryanjchen@yahoo.com>
2017-10-28 04:12:54 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser());
+
+app.get('/', function(req, res){
+	res.render('form');
+});
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.post('/', function(req, res){
+	console.log(req.body);
+	res.send("received your request!");
+});
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
