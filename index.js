329be5cfff8f42e38f8edfd1365aef693811e06c
Ryan Chen <ryanjchen@yahoo.com>
2017-10-28 07:43:29 -0500

    yeah https doesn't really work yet

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 var express = require('express');
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
+// var fs = require('fs');
+// var http = require('http');
+// var https = require('https');
 
-var privateKey  = fs.readFileSync('domain.key');
-var certificate = fs.readFileSync('domain.crt');
-var credentials = {
-	key: privateKey,
-	cert: certificate
-}
+// var privateKey  = fs.readFileSync('domain.key');
+// var certificate = fs.readFileSync('domain.crt');
+// var credentials = {
+// 	key: privateKey,
+// 	cert: certificate
+// }
 
 var app = express();
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://root:123@myhappytime-shard-00-00-mqmxc.mongodb.net:27017,myhappytime-shard-00-01-mqmxc.mongodb.net:27017,myhappytime-shard-00-02-mqmxc.mongodb.net:27017/happy_data?ssl=true&replicaSet=myhappytime-shard-0&authSource=admin"
 var randomstring = require('randomstring');
-var forceSsl = require('express-force-ssl');
 
 app.use(bodyParser());
-app.use(forceSsl);
 app.get('/', function(req, res){
 	res.render('form');
 });
@@ -54,9 +52,11 @@ app.post('/', function(req, res){
 
 
 
+app.listen(3000, function () {
+	console.log('App listening on port 3000!');
+});
+// var httpServer = http.createServer(app);
+// var httpsServer = https.createServer(credentials, app);
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(80, () => console.log("HTTP listening on 8080!"));
-httpsServer.listen(443, () => console.log("HTTPS listening on 8443!"));
+// httpServer.listen(80, () => console.log("HTTP listening on 8080!"));
+// httpsServer.listen(443, () => console.log("HTTPS listening on 8443!"));
