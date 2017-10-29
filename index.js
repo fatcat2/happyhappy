d76c7f9ef917dbc89eb968a2fdde817dc3f65cd7
Ryan Chen <ryanjchen@yahoo.com>
2017-10-29 01:52:34 -0500

    have to declare?

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ app.post('/textroommate', function(req, res){
 			if(result.length > 0){
 				db.collection("users").find({"group_code": result[0].group_code}).toArray(function(err, roomies){
 					if (err) throw err;
+					var roomie;
 					for roomie of roomies{
 						if(roomie.code != result[0].code){
 							client.messages.create({ 
