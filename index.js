362da802a16e2842a278d231393f793dd033bc58
Ryan Chen <ryanjchen@yahoo.com>
2017-10-29 01:50:58 -0500

    whew so much code

--- index.js
+++ index.js
@@ -33,29 +33,56 @@ app.post('/register', function(req, res){
 	  	length: 4,
 	  	charset: '123456789'
 	  })
-	  var group_id = "6969";
+
+	  //create ids, etc
+	  var group_id = confirm_code;
+	  var code1 = randomstring.generate({
+	  	length: 4,
+	  	charset: '123456789'
+	  });
+	  var code2 = randomstring.generate({
+	  	length: 4,
+	  	charset: '123456789'
+	  });
+
+	  //create the json objects
 	  var doc1 = {
 	  	"name": req.body.roomie1,
 	  	"number": req.body.roomie1_number,
 	  	"email": req.body.roomie1_email,
-	  	"code": "1111",
+	  	"code": code1,
 	  	"group_code": group_id
 	  }
 	  var doc2 = {
 	  	"name": req.body.roomie2,
 	  	"number": req.body.roomie2_number,
 	  	"email": req.body.roomie2_email,
-	  	"code": "0000",
+	  	"code": code2,
 	  	"group_code": group_id
 	  }
-	  db.collection("users").insertOne(doc1, function(err, res){
-	  	if (err) throw err;
-	  });
-	  db.collection("users").insertOne(doc2, function(err, res){
-	  	if (err) throw err;
-	  });
-	  console.log("Inserted " + doc1.name + " and " + doc2 + "'s info!\nWelcome!\n--------\n");
-	  db.close();
+
+		//text them their codes
+		client.messages.create({ 
+					to: req.body.roomie1_number,
+					from: twilio_num,
+					body: 'Hey, your unique code is ' + code1 + ', don\'t forget it!',
+				});
+		client.messages.create({ 
+					to: req.body.roomie2_number,
+					from: twilio_num,
+					body: 'Hey, your unique code is ' + code2 + ', don\'t forget it!',
+				});
+
+		//insert the docs
+		db.collection("users").insertOne(doc1, function(err, res){
+			if (err) throw err;
+		 });
+		db.collection("users").insertOne(doc2, function(err, res){
+			if (err) throw err;
+		});
+
+		console.log("Inserted " + doc1.name + " and " + doc2 + "'s info!\nWelcome!\n--------\n");
+		db.close();
 	});
 	res.render("goodbye");
 });
@@ -76,21 +103,22 @@ app.post('/textroommate', function(req, res){
 	// var _result 
 	MongoClient.connect(url, function(err, db){
 		if (err) throw err;
-		// console.log(req.body);
 		var x = req.body.key;
-		// console.log(x);
 		var y = x.split(',');
-		// console.log(y);
 		var code = y[1].replace(/\D/g, "");
-		// console.log(code);
 		db.collection("users").find({"code": code }).toArray(function(err, result){
 			if (err) throw err;
 			if(result.length > 0){
-				console.log(result[0])
-				client.messages.create({ 
-					to: result[0].number,
-					from: twilio_num,
-					body: 'Hey I need the room for a bit! Thanks for being patient!',
+				db.collection("users").find({"group_code": result[0].group_code}).toArray(function(err, roomie){
+					for x of roomie{
+						if(x.code != result[0].code){
+							client.messages.create({ 
+								to: x.number,
+								from: twilio_num,
+								body: 'Hey I need the room for a bit! Thanks for being patient!'\
+							});
+						}
+					}
 				})
 			}else{
 				console.log("Couldn't find it!");
